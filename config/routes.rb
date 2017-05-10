f538cc9225fa8daebaafb64f676270a46d5d4c7f
ningkai <ningkaiqunar@gmail.com>
2017-05-10 21:58:33 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  root 'welcome#index'
+  root 'products#index'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
