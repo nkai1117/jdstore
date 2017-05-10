da20def580aa2d8cb1d17d17dfc17febefb55df7
ningkai <ningkaiqunar@gmail.com>
2017-05-10 19:15:06 +0800

    implement backend CRUD

---
+++ db/migrate/20170510090919_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :string
+      t.text :description
+      t.string :price
+      t.string :integer
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510073721) do
+ActiveRecord::Schema.define(version: 20170510090919) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.string   "string"
+    t.text     "description"
+    t.string   "price"
+    t.string   "integer"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
