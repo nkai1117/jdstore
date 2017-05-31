92eb953d9fba1d3fd1a1f4feb1e540986a87fec7
ningkai <ningkaiqunar@gmail.com>
2017-05-31 18:42:59 +0800

    add order destroy

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -40,10 +40,10 @@ class Admin::OrdersController < ApplicationController
     redirect_to :back
   end
 
-#   def destroy
-#     @order = Order.find(params[:id])
-#     @order.destroy
-#   redirect_to admin_order_path
-# end
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to admin_orders_path
+  end
 
 end
