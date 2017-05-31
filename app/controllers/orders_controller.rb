6e4a5df358cf8302307f2ae0400c92e6647f63e6
ningkai <ningkaiqunar@gmail.com>
2017-05-31 12:52:57 +0800

    add aasm_state to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
+    @order.make_payment! # 用AASM的机制设定订单付款
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成支付"
   end
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment! # 用AASM的机制设定订单付款
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成支付"
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -19,5 +19,35 @@ class Order < ApplicationRecord
   def pay!
     self.update_columns(is_paid: true)
   end
-  
+
+  include AASM   #设定订单状态机制
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment, after_commit: :pay! do # 用AASM的机制设定订单付款
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid,         to: :shipping
+    end
+
+    event :deliver do
+      transitions from: :shipping,     to: :shipped
+  end
+
+  event :return_good do
+    transitions from: :shipped,        to: :good_returned
+  end
+
+  event :cancel_order do
+    transitions from: [:order_placed, :paid], to: :order_cancelled
+  end
+end
 end
